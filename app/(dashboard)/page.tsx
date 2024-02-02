ab111a31e553e054b66017c4763a1d8defd64002
aswanth6000 <aswanth682@gmail.com>
2024-02-02 17:24:48 +0530

    empty

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,6 +3,7 @@
 import { useOrganization } from "@clerk/nextjs";
 
 import { EmptyOrg } from "./_components/empty-org";
+import { BoardList } from "./board-list";
 
 interface DashboardPageProps {
   searchParams: {
@@ -18,7 +19,14 @@ const DashboardPage = ({
 
   return ( 
     <div className="flex-1 h-[calc(100%-80px)] p-6">
-      <EmptyOrg/>
+            {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList
+          orgId={organization.id}
+          query={searchParams}
+        />
+      )}
     </div>
    );
 };
