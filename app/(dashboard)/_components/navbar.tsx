4221f44df7f3b154ffefd60a5d02efdc2d973a6e
aswanth6000 <aswanth682@gmail.com>
2024-02-02 13:00:50 +0530

    nav bar done

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,17 +1,46 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, UserButton, useOrganization } from "@clerk/nextjs";
 import Searchinput from "./search-input";
+import { Invitebutton } from "./invite-button";
 
-const navbar = () => {
+const Navbar = () => {
+  const {organization} = useOrganization()
   return (
-    <div className="flex items center gap-y-4 p-5 ">
+    <div className="flex items-center gap-y-4 p-5 ">
        <div className="hidden lg:flex lg:flex-1 ">
         <Searchinput/>
        </div>
+       <div className="block lg:hidden flex-1">
+       <OrganizationSwitcher
+            hidePersonal
+            appearance={{
+                elements: {
+                    rootBox: {
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        width: "100%",
+                        maxWidth: "376px",
+                    },
+                    organizationSwitcherTrigger: {
+                        padding: "6px",
+                        width: "100%",
+                        borderRadius: "8px",
+                        border: "1px solid #E5E7EB",
+                        justifyContent: "space-between",
+                        backgroundColor: "white"
+                    }
+                }
+            }}
+            />
+       </div>
+       {organization && (
+        <Invitebutton/>
+       )}
        <UserButton/>
     </div>
   );
 };
 
-export default navbar;
+export default Navbar;
--- app/(dashboard)/_components/sidebar/item.tsx
+++ app/(dashboard)/_components/sidebar/item.tsx
@@ -1,44 +1,55 @@
-"use client"
-import Image from "next/image"
-import { useOrganization, useOrganizationList } from "@clerk/nextjs"
-import { cn } from "@/lib/utils"
-import { Hint } from "../hint"
+"use client";
 
-interface ItemProps{
-    id: string,
-    name: string,
-    imageUrl: string
-}
+import Image from "next/image";
+import {
+  useOrganization,
+  useOrganizationList,
+} from "@clerk/nextjs";
+
+import { cn } from "@/lib/utils";
+import { Hint } from "../hint";
+
+interface ItemProps {
+  id: string;
+  name: string;
+  imageUrl: string;
+};
 
 export const Item = ({
-    id,
-    name,
-    imageUrl,
-}: ItemProps) =>{
-    const {organization} = useOrganization();
-    const {setActive} = useOrganizationList();
-    const isActive = organization?.id === id
-    const onClick = () =>{
-        if(!setActive) return 
-        setActive({ organization: id})
-    }
-    return (
+  id,
+  name,
+  imageUrl,
+}: ItemProps) => {
+  const { organization } = useOrganization();
+  const { setActive } = useOrganizationList();
+
+  const isActive = organization?.id === id;
+
+  const onClick = () => {
+    if (!setActive) return;
+
+    setActive({ organization: id });
+  };
+
+  return (
     <div className="aspect-square relative">
-        <Hint label={name}
+      <Hint
+        label={name}
         side="right"
         align="start"
-        sideOffset={18}>
+        sideOffset={18}
+      >
         <Image
-        fill
-        alt={name}
-        src={imageUrl}
-        onClick={()=>{}}
-        className={cn(
+          fill
+          alt={name}
+          src={imageUrl}
+          onClick={onClick}
+          className={cn(
             "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
             isActive && "opacity-100"
-        )}
+          )}
         />
-        </Hint>
+      </Hint>
     </div>
-    )
-}
+  );
+};
