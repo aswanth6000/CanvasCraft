60f0e308156571f84e4497355d15d772777e5386
aswanth6000 <aswanth682@gmail.com>
2024-02-13 17:53:32 +0530

    participants done

--- app/board/_components/participants.tsx
+++ app/board/_components/participants.tsx
@@ -1,12 +1,55 @@
-export const Participants = () =>{
-    return (
-        <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
-            list of users
-        </div>
-    )
-}
+"use client";
+
+import { connectionIdToColor } from "@/lib/utils";
+import { useOthers, useSelf } from "@/liveblocks.config";
+
+import { UserAvatar } from "./user-avatar";
+
+const MAX_SHOWN_USERS = 2;
+
+export const Participants = () => {
+  const users = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOWN_USERS;
+
+  return (
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
+      <div className="flex gap-x-2">
+        {users.slice(0, MAX_SHOWN_USERS)
+          .map(({ connectionId, info }) => {
+            return (
+              <UserAvatar
+                borderColor={connectionIdToColor(connectionId)}
+                key={connectionId}
+                src={info?.picture}
+                name={info?.name}
+                fallback={info?.name?.[0] || "T"}
+              />
+            )
+        })}
+        
+        {currentUser && (
+          <UserAvatar
+            borderColor={connectionIdToColor(currentUser.connectionId)}
+            src={currentUser.info?.picture}
+            name={`${currentUser.info?.name} (You)`}
+            fallback={currentUser.info?.name?.[0]}
+          />
+        )}
+
+        {hasMoreUsers && (
+          <UserAvatar
+            name={`${users.length - MAX_SHOWN_USERS} more`}
+            fallback={`+${users.length - MAX_SHOWN_USERS}`}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
 export const ParticipantsSkeleton = () => {
-    return (
-      <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]" />
-    );
-  };
+  return (
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]" />
+  );
+};
